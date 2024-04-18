10bdfd2693f21e67ae155f6e7ecf0b9110ab41d8
Mauro Chojrin <mauro.chojrin@codurance.com>
2024-04-18 12:04:48 +0200

    Program to use the class and generate the expected output

--- TwelveDaysOfChristmasSongApp/Program.cs
+++ TwelveDaysOfChristmasSongApp/Program.cs
@@ -6,9 +6,13 @@ Console.WriteLine("Enter a day number between 1 and 12");
 var day = Console.In.ReadLine();
 var output = VerseGenerator.GenerateFor(Int32.Parse(day));
 
-Console.WriteLine(string.Join(",\n", output.ToArray()));
-
+Console.WriteLine(string.Join("\n", output.GetRange(0, 2).ToArray()));
+Console.WriteLine(string.Join(",\n", output.GetRange(2, output.Count - 3)));
 if (output.Count > 3)
 {
-    
+    Console.WriteLine("And " + output.Last().ToLower());
+}
+else
+{
+    Console.WriteLine(output.Last());
 }
